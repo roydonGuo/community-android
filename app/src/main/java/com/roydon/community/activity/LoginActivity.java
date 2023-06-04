5641165d5d956f13e87664e4793c258bfede6d68
roydonGuo <3133010060@qq.com>
2023-06-04 23:16:41 +0800

    首页完善-添加loginActivity

---
+++ app/src/main/java/com/roydon/community/activity/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.roydon.community.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.roydon.community.R;
+
+/**
+ * @author roydon
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
