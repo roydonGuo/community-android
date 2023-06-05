25879174414822451daf4304babd67ddf0e3bc62
roydonGuo <3133010060@qq.com>
2023-06-06 02:03:34 +0800

    登录-注册页面

--- app/src/main/java/com/roydon/community/MainActivity.java
+++ app/src/main/java/com/roydon/community/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.roydon.community.activity.LoginActivity;
+import com.roydon.community.activity.RegisterActivity;
 
 /**
  * @author roydon
@@ -24,10 +25,20 @@ public class MainActivity extends AppCompatActivity {
         buttonLogin = findViewById(R.id.button_login);
         buttonRegister = findViewById(R.id.button_register);
 
+        // 跳转登陆页面
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 跳转注册页面
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
