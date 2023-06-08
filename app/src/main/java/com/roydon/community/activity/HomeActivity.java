e5197c36e9d6ca6318592f1fe3ff54a43b7b266d
roydon <3133010060@qq.com>
2023-06-08 15:21:06 +0800

    底部导航栏homepage

---
+++ app/src/main/java/com/roydon/community/activity/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.roydon.community.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.roydon.community.R;
+
+/**
+ * @author roydon
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
