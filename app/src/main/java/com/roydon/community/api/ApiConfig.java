fb1f340e0b2d7f304b3c2c6b73aecc0f77882080
roydonGuo <3133010060@qq.com>
2023-06-08 19:07:58 +0800

    api配置

---
+++ app/src/main/java/com/roydon/community/api/ApiConfig.java
@@ -0,0 +1,8 @@
+package com.roydon.community.api;
+
+public class ApiConfig {
+    public static final int PAGE_SIZE = 5;
+    public static final String BASE_URl = "http://106.14.105.101:8088";
+
+    public static final String LOGIN = "/app/login"; //登录
+}
---
+++ app/src/main/java/com/roydon/community/config/AppConfig.java
@@ -0,0 +1,5 @@
+package com.roydon.community.config;
+
+public class AppConfig {
+    public static final String BASE_URl = "http://106.14.105.101:8088/app";
+}
