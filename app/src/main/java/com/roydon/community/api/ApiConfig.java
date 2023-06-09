ed7989568c365e5b701b4b4dd62ad04bbdad0920
roydon <3133010060@qq.com>
2023-06-09 16:32:05 +0800

    新闻集合接口

--- app/src/main/java/com/roydon/community/api/ApiConfig.java
+++ app/src/main/java/com/roydon/community/api/ApiConfig.java
@@ -2,7 +2,7 @@ package com.roydon.community.api;
 
 public class ApiConfig {
 
-    public static final int PAGE_SIZE = 5;
+    public static final int PAGE_SIZE = 10;
 //        public static final String BASE_URl = "http://106.14.105.101:8088";
     public static final String BASE_URl = "http://192.168.1.25:8088";
 
