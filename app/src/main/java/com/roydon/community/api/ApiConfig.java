7cb3c5da9c617129db5f4870f07660d43f2ea0a5
roydon <3133010060@qq.com>
2023-06-09 15:30:32 +0800

    优化-新闻页面

--- app/src/main/java/com/roydon/community/api/ApiConfig.java
+++ app/src/main/java/com/roydon/community/api/ApiConfig.java
@@ -1,9 +1,21 @@
 package com.roydon.community.api;
 
 public class ApiConfig {
-    public static final int PAGE_SIZE = 5;
-    public static final String BASE_URl = "http://106.14.105.101:8088";
 
+    public static final int PAGE_SIZE = 5;
+//        public static final String BASE_URl = "http://106.14.105.101:8088";
+    public static final String BASE_URl = "http://192.168.1.25:8088";
+
+    /**
+     * app前缀接口
+     */
     public static final String LOGIN = "/app/login"; //登录
     public static final String REGISTER = "/app/register"; //注册
+
+    /**
+     * 新闻模块接口
+     */
+    public static final String NEWS_CATEGORY_LIST = "/app/news/category"; //新闻分类
+    public static final String NEWS_LIST = "/app/news/list"; //新闻分类
+
 }
--- app/src/main/java/com/roydon/community/fragment/NewsFragment.java
+++ app/src/main/java/com/roydon/community/fragment/NewsFragment.java
@@ -6,47 +6,180 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
+import com.flyco.tablayout.SlidingTabLayout;
+import com.google.gson.Gson;
 import com.roydon.community.R;
+import com.roydon.community.adapter.HomeAdapter;
+import com.roydon.community.adapter.NewsAdapter;
+import com.roydon.community.api.Api;
+import com.roydon.community.api.ApiConfig;
+import com.roydon.community.api.HttpCallback;
+import com.roydon.community.entity.AppNews;
+import com.roydon.community.entity.NewsCategoryRes;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
-public class NewsFragment extends Fragment {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class NewsFragment extends BaseFragment {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private String[] mTitles;
+    private ViewPager viewPager;
+    private SlidingTabLayout slidingTabLayout;
+    private RecyclerView recyclerView;
+    private RefreshLayout refreshLayout;
+    private NewsAdapter newsAdapter;
+    private LinearLayoutManager linearLayoutManager;
 
-    public NewsFragment() {
-        // Required empty public constructor
+    private Long dictCode;
+    private int pageNum = 1;
+    private List<AppNews> newsList = new ArrayList<>();
+
+    public static NewsFragment newInstance(Long dictCode) {
+        NewsFragment fragment = new NewsFragment();
+        fragment.dictCode = dictCode;
+        return fragment;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     * @return A new instance of fragment NewsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    public NewsFragment() {
+    }
+
     public static NewsFragment newInstance() {
         return new NewsFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    protected int initLayout() {
+        return R.layout.fragment_news;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+    protected void initView() {
+        // 绑定fixedViewPager待渲染数据
+        viewPager = mRootView.findViewById(R.id.fixedViewPager);
+        // 绑定顶部分类导航
+        slidingTabLayout = mRootView.findViewById(R.id.slidingTabLayout);
+    }
+
+    @Override
+    protected void initData() {
+        this.getNewsCategoryList();
+//        linearLayoutManager = new LinearLayoutManager(getActivity());
+//        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+//        recyclerView.setLayoutManager(linearLayoutManager);
+//        newsAdapter = new NewsAdapter(getActivity());
+//        recyclerView.setAdapter(newsAdapter);
+//        newsAdapter.setOnItemClickListener(new NewsAdapter.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(Serializable obj) {
+////                showToast("点击");
+//                AppNews newsEntity = (AppNews) obj;
+//                String url = "http://192.168.31.32:8089/newsDetail?title=" + newsEntity.getAuthorName();
+//                Bundle bundle = new Bundle();
+//                bundle.putString("url", url);
+//                navigateToWithBundle(WebActivity.class, bundle);
+//            }
+//        });
+//        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+//            @Override
+//            public void onRefresh(RefreshLayout refreshlayout) {
+//                pageNum = 1;
+//                getNewsList(true);
+//            }
+//        });
+//        refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+//            @Override
+//            public void onLoadMore(RefreshLayout refreshlayout) {
+//                pageNum++;
+//                getNewsList(false);
+//            }
+//        });
+//        getNewsList(true);
+    }
+//    private void getNewsList(final boolean isRefresh){
+//        HashMap<String, Object> params = new HashMap<>();
+//        params.put("page", pageNum);
+//        params.put("limit", ApiConfig.PAGE_SIZE);
+//        Api.config(ApiConfig.NEWS_LIST, params).getRequest(getActivity(), new TtitCallback() {
+//            @Override
+//            public void onSuccess(final String res) {
+//                if (isRefresh) {
+//                    refreshLayout.finishRefresh(true);
+//                } else {
+//                    refreshLayout.finishLoadMore(true);
+//                }
+//                NewsListResponse response = new Gson().fromJson(res, NewsListResponse.class);
+//                if (response != null && response.getCode() == 0) {
+//                    List<NewsEntity> list = response.getPage().getList();
+//                    if (list != null && list.size() > 0) {
+//                        if (isRefresh) {
+//                            datas = list;
+//                        } else {
+//                            datas.addAll(list);
+//                        }
+//                        mHandler.sendEmptyMessage(0);
+//                    } else {
+//                        if (isRefresh) {
+//                            showToastSync("暂时无数据");
+//                        } else {
+//                            showToastSync("没有更多数据");
+//                        }
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Exception e) {
+//                if (isRefresh) {
+//                    refreshLayout.finishRefresh(true);
+//                } else {
+//                    refreshLayout.finishLoadMore(true);
+//                }
+//            }
+//        });
+//    }
+
+    /**
+     * 新闻分类集合
+     */
+    private void getNewsCategoryList() {
+        HashMap<String, Object> params = new HashMap<>();
+        Api.config(ApiConfig.NEWS_CATEGORY_LIST, params).getRequest(getActivity(), new HttpCallback() {
+            @Override
+            public void onSuccess(final String res) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        NewsCategoryRes response = new Gson().fromJson(res, NewsCategoryRes.class);
+                        if (response != null && response.getCode() == 200) {
+                            List<NewsCategoryRes.DataBean> data = response.getData();
+                            if (data != null && data.size() > 0) {
+                                mTitles = new String[data.size()];
+                                for (int i = 0; i < data.size(); i++) {
+                                    mTitles[i] = data.get(i).getDictLabel();
+                                    mFragments.add(NewsFragment.newInstance(data.get(i).getDictCode()));
+                                }
+                                viewPager.setOffscreenPageLimit(mFragments.size());
+                                viewPager.setAdapter(new HomeAdapter(getFragmentManager(), mTitles, mFragments));
+                                slidingTabLayout.setViewPager(viewPager);
+                            }
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+            }
+        });
     }
 }
