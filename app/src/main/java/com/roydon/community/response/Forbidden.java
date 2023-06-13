182ab061d5d8d3791801ed0188d52077562da6f1
roydonGuo <3133010060@qq.com>
2023-06-13 13:56:11 +0800

    优化-403实体

---
+++ app/src/main/java/com/roydon/community/response/Forbidden.java
@@ -0,0 +1,10 @@
+package com.roydon.community.response;
+
+/**
+ * @author roydon
+ * @date 2023/6/13 11:58
+ * 403:{"timestamp":"2023-06-13T11:56:10.548+08:00","status":403,"error":"Forbidden","path":"/app/news/category"}
+ */
+public class Forbidden {
+
+}
