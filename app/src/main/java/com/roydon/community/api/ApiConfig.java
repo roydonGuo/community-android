e100e79b4e067d464989f32ba78fd16182ab1a6b
roydonGuo <3133010060@qq.com>
2023-06-13 13:57:18 +0800

    优化-请求头

--- app/src/main/java/com/roydon/community/api/ApiConfig.java
+++ app/src/main/java/com/roydon/community/api/ApiConfig.java
@@ -3,8 +3,8 @@ package com.roydon.community.api;
 public class ApiConfig {
 
     public static final int PAGE_SIZE = 10;
-//        public static final String BASE_URl = "http://106.14.105.101:8088";
-    public static final String BASE_URl = "http://192.168.1.25:8088";
+        public static final String BASE_URl = "http://106.14.105.101:8088";
+//    public static final String BASE_URl = "http://192.168.1.25:8088";
 
     /**
      * app前缀接口
@@ -16,6 +16,6 @@ public class ApiConfig {
      * 新闻模块接口
      */
     public static final String NEWS_CATEGORY_LIST = "/app/news/category"; //新闻分类
-    public static final String NEWS_LIST = "/app/news/list"; //新闻分类
+    public static final String NEWS_LIST = "/app/news/list"; //新闻list
 
 }
