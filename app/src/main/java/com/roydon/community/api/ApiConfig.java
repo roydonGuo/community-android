4c8a53de0bdab0510564d091204cbb34030c9fc9
roydonGuo <3133010060@qq.com>
2023-06-18 21:08:13 +0800

    improve-add news detail

--- app/src/main/java/com/roydon/community/api/ApiConfig.java
+++ app/src/main/java/com/roydon/community/api/ApiConfig.java
@@ -4,7 +4,7 @@ public class ApiConfig {
 
     public static final int PAGE_SIZE = 10;
         public static final String BASE_URl = "http://106.14.105.101:8088";
-//    public static final String BASE_URl = "http://192.168.1.25:8088";
+//    public static final String BASE_URl = "http://192.168.0.101:8088";
 
     /**
      * app前缀接口
@@ -17,5 +17,6 @@ public class ApiConfig {
      */
     public static final String NEWS_CATEGORY_LIST = "/app/news/category"; //新闻分类
     public static final String NEWS_LIST = "/app/news/list"; //新闻list
+    public static final String NEWS_DETAIL = "/app/news/detail"; //新闻detail
 
 }
