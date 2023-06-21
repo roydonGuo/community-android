8f65daa126b5b6395761213000fc765ad62c7ca6
roydonGuo <3133010060@qq.com>
2023-06-22 02:02:28 +0800

    improve-欢迎页

--- app/src/main/java/com/roydon/community/activity/WelcomeActivity.java
+++ app/src/main/java/com/roydon/community/activity/WelcomeActivity.java
@@ -1,16 +1,35 @@
 package com.roydon.community.activity;
 
-import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.roydon.community.BaseActivity;
+import com.roydon.community.MainActivity;
 import com.roydon.community.R;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+    protected int initLayout() {
+        return R.layout.activity_welcome;
     }
+
+    @Override
+    protected void initView() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+
 }
