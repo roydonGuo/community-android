0cc2b2164d1ff13e9875aae5074ecfd439f8ca2f
roydon <3133010060@qq.com>
2023-07-30 17:44:06 +0800

    创建订单响应体

--- app/src/main/java/com/roydon/community/domain/vo/OrderCreateRes.java
+++ app/src/main/java/com/roydon/community/domain/vo/OrderCreateRes.java
@@ -1,13 +1,14 @@
 package com.roydon.community.domain.vo;
 
 import com.roydon.community.domain.entity.MallOrder;
+import com.roydon.community.domain.response.BaseResponse;
 
 /**
  * @author roydon
  * @date 2023/6/25 16:35
  * @description community-android
  */
-public class OrderCreateRes extends BaseResponse{
+public class OrderCreateRes extends BaseResponse {
     private MallOrder data;
 
     public OrderCreateRes() {
