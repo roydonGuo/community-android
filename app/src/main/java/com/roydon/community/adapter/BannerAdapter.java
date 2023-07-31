6216146269f67462db6339e1c9b80d1aad646726
roydon <3133010060@qq.com>
2023-07-31 16:52:16 +0800

    Banner图片本地缓存

--- app/src/main/java/com/roydon/community/adapter/BannerAdapter.java
+++ app/src/main/java/com/roydon/community/adapter/BannerAdapter.java
@@ -1,6 +1,7 @@
 package com.roydon.community.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,7 +9,7 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
-import com.bumptech.glide.Glide;
+import com.roydon.community.utils.img.MyBitmapUtils;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ public class BannerAdapter extends PagerAdapter {
 
     private Context mContext;
     private List<String> mUrls;
+    private MyBitmapUtils myBitmapUtils;
 
     public BannerAdapter() {
     }
@@ -60,7 +62,11 @@ public class BannerAdapter extends PagerAdapter {
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         ImageView imageView = new ImageView(mContext);
         // 让图片填充至整个控件，只需要添加centerCrop()
-        Glide.with(mContext).load(mUrls.get(position)).centerCrop().into(imageView);
+//        Glide.with(mContext).load(mUrls.get(position)).centerCrop().into(imageView);
+        myBitmapUtils = new MyBitmapUtils(mContext);
+        Bitmap imageFile = myBitmapUtils.disPlay(imageView, mUrls.get(position));
+        imageView.setImageBitmap(imageFile);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
         // 图片加载过程中显示指定以图片
 //        Glide.with(context).load(url).centerCrop().placeholder(R.mipmap.defaultpic).into(imageView);
         // 图片加载失败同理，使用error();
